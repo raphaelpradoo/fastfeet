c1d9d2cb5c938516dfb6dfe6b6b4f33a448bea37
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-07 15:11:21 -0300

    Frontend - Troca de Repositório

---
+++ frontend/src/pages/Delivery/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: green;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const Title = styled.div``;
+
+export const Actions = styled.div``;
+
+export const Find = styled.div``;
+
+export const Add = styled.div``;
