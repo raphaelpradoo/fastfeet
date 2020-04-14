af392e7902887d9d8113279aa7cb4ceaebfd4ecc
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-14 17:51:43 -0300

    Exclusao do pProjeto Mobile

--- mobile/src/pages/Dashboard/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-
-// import { Container } from './styles';
-
-export default function Dashboard() {
-  return <SafeAreaView>Dashboard</SafeAreaView>;
-}
--- mobile/src/pages/Profile/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-
-// import { Container } from './styles';
-
-export default function Profile() {
-  return <SafeAreaView>Profile</SafeAreaView>;
-}
