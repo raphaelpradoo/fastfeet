09156b71fe0cebe74c7f4c3c846e732a79f1c703
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-15 11:29:43 -0300

    Layout da Tela de Login

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image, StatusBar } from 'react-native';
+import { Form } from '@unform/mobile';
 
-import { Container } from './styles';
+import logo from '~/assets/logo.png';
+import colors from '~/styles/colors';
+import { Container, Input, SubmitButton } from './styles';
 
 export default function SignIn() {
   return (
     <Container>
-      <View>
-        <Text>Login</Text>
-      </View>
+      <StatusBar backgroundColor={colors.primary} />
+      <Image source={logo} />
+      <Form>
+        <Input
+          name="id"
+          keyboardType="default"
+          placeholder="Informe seu ID de cadastro"
+          autoCorrect={false}
+          returnKeyType="send"
+          autoCapitalize="none"
+        />
+        <SubmitButton>Entrar no sistema</SubmitButton>
+      </Form>
     </Container>
   );
 }
--- mobile/src/pages/SignIn/styles.js
+++ mobile/src/pages/SignIn/styles.js
@@ -1,20 +1,24 @@
-import { Platform } from 'react-native';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
-export const Container = styled.KeyboardAvoidingView.attrs({
-  enabled: Platform.OS === 'ios',
-  behavior: 'padding',
-})`
+import Button from '~/components/Button';
+import UnInput from '~/components/Input';
+
+export const Container = styled.View`
+  background: #7d40e7;
   flex: 1;
-  background: #7159c1;
+
   justify-content: center;
   align-items: center;
-  padding: 0 30px;
+
+  padding: 0 25px;
 `;
 
-/*
-export const Form = styled.View`
-  align-self: stretch;
-  margin-top: 50px;
+export const Input = styled(UnInput)`
+  margin-top: 37.5px;
 `;
-*/
+
+export const SubmitButton = styled(Button)`
+  background: #82bf18;
+  width: 100%;
+  margin-top: 15.5px;
+`;
