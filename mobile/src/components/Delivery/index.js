6bfa479abd91083aedb643eaedfe8ab9fb93f8e5
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-17 07:53:07 -0300

    Tela de Detalhes da Encomenda

--- mobile/src/components/Delivery/index.js
+++ mobile/src/components/Delivery/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 
 import Status from '~/components/Status';
@@ -17,20 +17,20 @@ import {
   TextLink,
 } from './styles';
 
-export default function Delivey({ data }) {
-  // const navigation = useNavigation();
+export default function Delivery({ data }) {
+  const navigation = useNavigation();
 
-  function checkStatus(deliveryItem) {
-    if (deliveryItem.canceled_at !== null) {
+  function checkStatus(delivery) {
+    if (delivery.canceled_at !== null) {
       return 'CANCELADA';
     }
-    if (deliveryItem.start_date === null) {
+    if (delivery.start_date === null) {
       return 'PENDENTE';
     }
-    if (deliveryItem.start_date !== null && deliveryItem.end_date === null) {
+    if (delivery.start_date !== null && delivery.end_date === null) {
       return 'RETIRADA';
     }
-    if (deliveryItem.end_date !== null) {
+    if (delivery.end_date !== null) {
       return 'ENTREGUE';
     }
     return '';
@@ -58,8 +58,7 @@ export default function Delivey({ data }) {
         </Detail>
         <Detail>
           <TextLink
-            // onPress={() => navigation.navigate('Detalhes', { delivery: data })}
-            onPress={() => {}}
+            onPress={() => navigation.navigate('Detalhes', { delivery: data })}
           >
             Ver detalhes
           </TextLink>
@@ -69,7 +68,7 @@ export default function Delivey({ data }) {
   );
 }
 
-Delivey.propTypes = {
+Delivery.propTypes = {
   data: PropTypes.shape({
     id: PropTypes.number.isRequired,
     status: PropTypes.string.isRequired,
---
+++ mobile/src/pages/DeliveryDetails/index.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import { Alert, StatusBar, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+import colors from '~/styles/colors';
+
+import {
+  Container,
+  Background,
+  Content,
+  Card,
+  TitleContainer,
+  Title,
+  Label,
+  Value,
+  Status,
+  Menu,
+  Option,
+  OptionTitle,
+} from './styles';
+
+export default function DeliveryDetails() {
+  const auth = useSelector((state) => state.auth);
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { delivery } = route.params;
+
+  async function handleDeliveryReceive() {
+    async function delievryReceive() {
+      try {
+        await api.put(`/deliveryman/${auth.id}/delivery/${delivery.id}`, {
+          start_date: new Date(),
+        });
+
+        navigation.navigate('Entregas');
+      } catch (err) {
+        Alert.alert('Horário de retirda inválida.');
+      }
+    }
+
+    Alert.alert(
+      'Confirmação de retirada',
+      'Confirma que deseja realizar a retirada desta encomenda?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'destructive',
+        },
+        {
+          text: 'Confirmar',
+          onPress: delievryWithdraw,
+        },
+      ],
+      {
+        cancelable: false,
+      }
+    );
+  }
+
+  function checkStatus(delivery) {
+    if (delivery.canceled_at !== null) {
+      return 'CANCELADA';
+    }
+    if (delivery.start_date === null) {
+      return 'PENDENTE';
+    }
+    if (delivery.start_date !== null && delivery.end_date === null) {
+      return 'RETIRADA';
+    }
+    if (delivery.end_date !== null) {
+      return 'ENTREGUE';
+    }
+    return '';
+  }
+
+  const status = checkStatus(delivery);
+
+  return (
+    <Container>
+      <StatusBar backgroundColor={colors.primary} barStyle="light-content" />
+      <Background />
+      <Content>
+        <Card>
+          <TitleContainer>
+            <Icon name="local-shipping" color={colors.primary} size={20} />
+            <Title>Informações da entrega</Title>
+          </TitleContainer>
+          <Label>DESTINATÁRIO</Label>
+          <Value>{delivery.recipient.name}</Value>
+          <Label>ENDEREÇO DE ENTREGA</Label>
+          <Value>
+            {delivery.recipient.address}, {delivery.recipient.number},{' '}
+            {delivery.recipient.city} - {delivery.recipient.state},{' '}
+            {delivery.recipient.cep}
+          </Value>
+          <Label>PRODUTO</Label>
+          <Value>{delivery.product}</Value>
+        </Card>
+
+        <Card>
+          <TitleContainer>
+            <Icon name="event" color={colors.primary} size={20} />
+            <Title>Situação da entrega</Title>
+          </TitleContainer>
+          <Label>STATUS</Label>
+          <Status>{status}</Status>
+          <View
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <View>
+              <Label>DATA DE RETIRADA</Label>
+              <Value>{delivery.start_date_formated}</Value>
+            </View>
+            <View>
+              <Label>DATA DE ENTREGA</Label>
+              <Value>{delivery.end_date_formated}</Value>
+            </View>
+          </View>
+        </Card>
+
+        <Menu>
+          <Option
+            onPress={() =>
+              navigation.navigate('CraeteProblem', { delivery_id: delivery.id })
+            }
+          >
+            <Icon name="highlight-off" color={colors.danger} size={20} />
+            <OptionTitle>Informar{`\n`}Problema</OptionTitle>
+          </Option>
+
+          <Option>
+            <Icon name="info-outline" color="#E7BA40" size={20} />
+            <OptionTitle>Visualizar{`\n`}Problemas</OptionTitle>
+          </Option>
+
+          {status === 'PENDENTE' ? (
+            <Option onPress={handleDeliveryReceive}>
+              <Icon name="local-shipping" color={colors.primary} size={20} />
+              <OptionTitle>Realizar{`\n`}Retirada</OptionTitle>
+            </Option>
+          ) : (
+            <Option
+              onPress={() =>
+                navigation.navigate('ConfirmPhoto', {
+                  delivery_id: delivery.id,
+                })
+              }
+            >
+              <Icon name="check-circle" color={colors.primary} size={20} />
+              <OptionTitle>Confirmar{`\n`}Entrega</OptionTitle>
+            </Option>
+          )}
+        </Menu>
+      </Content>
+    </Container>
+  );
+}
